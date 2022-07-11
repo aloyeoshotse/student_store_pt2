76520fc165263558aa35f63dfdb30d760481ed5e
Aloye Oshotse <aloye.oshotse@gmail.com>
2022-07-11 14:03:25 -0700

    created JWT utils and security middleware

--- student_store_backend/config.js
+++ student_store_backend/config.js
@@ -2,8 +2,11 @@ require("dotenv").config()
 require("colors")
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3001
+const SECRET_KEY = process.env.SECRET_KEY || 'secret_dev'
 
 const IS_TESTING = process.env.NODE_ENV === "test"
+
+
 
 function getDatabaseUri() {
   const dbUser = process.env.DATABASE_USER || "postgres"
@@ -22,6 +25,7 @@ const BCRYPT_WORK_FACTOR = IS_TESTING ? 4 : 13
 
 console.log("Student Store Config:".red)
 console.log("PORT:".blue, PORT)
+console.log("SECRET_KEY:".blue, SECRET_KEY)
 console.log("IS_TESTING:".blue, IS_TESTING)
 console.log("BCRYPT_WORK_FACTOR".blue, BCRYPT_WORK_FACTOR)
 console.log("Database:".blue, getDatabaseUri())
@@ -29,6 +33,7 @@ console.log("---")
 
 module.exports = {
   PORT,
+  SECRET_KEY,
   IS_TESTING,
   BCRYPT_WORK_FACTOR,
   getDatabaseUri,
