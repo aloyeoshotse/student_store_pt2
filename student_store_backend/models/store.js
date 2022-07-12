29850fffce7eb98a30394e7a75e9a0fbac089423
Aloye Oshotse <aloye.oshotse@gmail.com>
2022-07-11 19:36:36 -0700

    completed Store model and routes

--- student_store_backend/models/store.js
+++ student_store_backend/models/store.js
@@ -1,10 +1,36 @@
-const db = require('../config');
+const db = require('../db');
 
 class Store {
     
     static async listProducts() {
         //run a SQL query that searches the database for all products and returns a list of them
+        const results = await db.query(
+            `
+             SELECT * FROM products;
+            `
+        );
+        console.log("results: ", results.rows)
+        return results.rows
+
+
+        // const results = await db.query(
+        //     `
+        //      SELECT orders.id AS "orderId",
+        //             orders.customer_id AS "customerId",
+        //             order_details.quantity AS "quantity",
+        //             products.name AS "name",
+        //             products.price AS "price"
+        //      FROM orders
+        //      JOIN order_details ON orders.id = order_details.order_id
+        //      JOIN products ON products.id = order_details.product_id
+        //      WHERE customer_id = (SELECT id FROM users WHERE email = $1);
+        //     `, [user.email]
+        // )
+      
+        // return results.rows
     }
 }
+
+
 
 module.exports = Store
--- student_store_backend/routes/store.js
+++ student_store_backend/routes/store.js
@@ -2,9 +2,9 @@ const express = require("express")
 const Store = require("../models/store")
 const router = express.Router()
 
-router.get('/', (req,res,next) => {
+router.get('/', async (req,res,next) => {
     try {
-        let productList = Store.listProducts;
+        let productList = await Store.listProducts();
         res.status(200).json({
             products : productList
         })
